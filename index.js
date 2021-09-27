8f14c54107b1cc44b36a019bbfe182bee017e418
vegsemd <maxshout24@gmail.com>
2021-09-27 19:48:50 +0600

    modified index.js

--- index.js
+++ index.js
@@ -10,3 +10,11 @@ for (let i = 0; i < 5; i++) {
 	console.log(i);
 }
 
+const arr = [];
+const arrSize = 10;
+
+for (let i = 0; i < arrSize; i++) {
+	arr[i] = Math.random() * 10;
+	if (arr[i] % 2 === 0) console.log(`Even value! ${arr[i]}`);
+}
+
