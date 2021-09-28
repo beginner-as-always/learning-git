aab28d034436522bfe0d87ca580a625f222f3223
vegsemd <maxshout24@gmail.com>
2021-09-28 18:28:25 +0600

    bubble sort

--- arrays.js
+++ arrays.js
@@ -17,3 +17,15 @@ for (let i = 0; i < size; ) {
         console.log(`${i + 1}: ${(array[i++] = candidate)}`);
 }
 
+console.log('Array is now filled with unique random values');
+
+for (let i = 0; i < size; i++) {
+    for (let j = 1; j < i; j++) {
+        if (array[j - 1] > array[j]) {
+            const temp = array[j - 1];
+            array[j - 1] = array[j];
+            array[j] = temp;
+        }
+    }
+}
+
